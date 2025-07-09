41d213ce8186076c5ee1197b21893f872739377d
Fun-Times91 <bayce91@gmail.com>
2025-07-09 20:46:16 +0200

    route 'les comptes' inutile suppmrimer

--- frontend/public/Router/allRoutes.js
+++ frontend/public/Router/allRoutes.js
@@ -9,7 +9,6 @@ export const allRoutes = [
     new Route("/habitats", "Nos Habitats", "/pages/habitat/habitats.html", [], "/js/habitat/habitats.js"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
     new Route("/contact", "Contact", "/pages/contact.html", [], "/js/contact/contact.js"),
-    new Route("/account", "Les comptes", "/pages/auth/account.html", ["ROLE_ADMIN"]),
     new Route("/adminDashboard", "Dashboard Admin", "/pages/admin/dashboardAdmin.html", ["ROLE_ADMIN"], "/js/admin/dashboardAdmin.js"),
     new Route("/employeeDashboard", "Dashboard Employé", "/pages/employee/dashboardEmployee.html", ["ROLE_EMPLOYEE"], "/js/employee/dashboardEmployee.js"),
 ];
